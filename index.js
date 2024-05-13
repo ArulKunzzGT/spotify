fde8d66dee2ae65fbce11917c9a2d9252f5838e1
ArulKunzzGT <arulgemer93@gmail.com>
2024-05-13 21:24:23 +0700

    Initial commit

--- index.js
+++ index.js
@@ -1,16 +1,26 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 const PORT = 3000;
 
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, 'public')));
+app.get('/home', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'home.html'));
+});
 
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/public/index.html');
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+}); 
+
+app.use((req, res) => {
+    res.status(404).sendFile(path.join(__dirname, 'public', '404.html'));
 });
 
-app.get('/home', (req, res) => {
-    res.sendFile(__dirname + '/public/home.html')
-});
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).sendFile(path.join(__dirname, 'public', '500.html'));
+ });
+
 
 app.listen(PORT, () => {
     console.log(`Server berjalan di http://localhost:${PORT}`);
