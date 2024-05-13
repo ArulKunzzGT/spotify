30f8e77fb0dcaa7fcb826010ff0c40fc1f648504
ArulKunzzGT <arulgemer93@gmail.com>
2024-05-13 19:49:05 +0700

    SPOTIFY

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/public/index.html');
+});
+
+app.get('/home', (req, res) => {
+    res.sendFile(__dirname + '/public/home.html')
+});
+
+app.listen(PORT, () => {
+    console.log(`Server berjalan di http://localhost:${PORT}`);
+});
